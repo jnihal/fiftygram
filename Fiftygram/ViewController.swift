7746402dad4d199915e26aabb2895a1464051e81
Nihal Jain <nihaljain222@gmail.com>
2021-02-21 13:14:56 +0530

    First commit

---
+++ Fiftygram/ViewController.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+
+    let context = CIContext()
+    var original: UIImage!
+
+    @IBAction func applySepia() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        filter?.setValue(1.0, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func applyNoir() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func applyVintage() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+    
+    @IBAction func applyInvert() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIColorInvert")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+
+    @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            self.navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func savePhoto() {
+        let alert = UIAlertController(title: "", message: "Would you like to save the image?", preferredStyle: UIAlertController.Style.alert)
+
+        alert.addAction(UIAlertAction(title: "Save", style: UIAlertAction.Style.default, handler: { _ in
+            if self.imageView.image == nil {
+                return
+            }
+            UIImageWriteToSavedPhotosAlbum(self.imageView.image!, nil, nil, nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func display(filter: CIFilter) {
+        let output = filter.outputImage!
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output, from: output.extent)!)
+    }
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        self.navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
+}
